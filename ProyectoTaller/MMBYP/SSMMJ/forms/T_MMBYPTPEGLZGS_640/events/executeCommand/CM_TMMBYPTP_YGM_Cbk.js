5d67393da0bc60e9e2d65f74fb4f1f181916c4eb
Harold Flores <bhflores1994@gmail.com>
2020-12-21 18:03:28 -0500

    se añaden eventos a callback para que refresque y cierre pantalla

--- ProyectoTaller/MMBYP/SSMMJ/forms/T_MMBYPTPEGLZGS_640/events/executeCommand/CM_TMMBYPTP_YGM_Cbk.js
+++ ProyectoTaller/MMBYP/SSMMJ/forms/T_MMBYPTPEGLZGS_640/events/executeCommand/CM_TMMBYPTP_YGM_Cbk.js
@@ -2,4 +2,8 @@
 //Start signature to Callback event to CM_TMMBYPTP_YGM
 task.executeCommandCallback.CM_TMMBYPTP_YGM = function(entities, executeCommandCallbackEventArgs) {
      //here your code
+    executeCommandCallbackEventArgs.commons.api.grid.refresh('QV_IP48_CFD88');
+    executeCommandCallbackEventArgs.commons.api.navigation.closeModal({});
+
+
 };

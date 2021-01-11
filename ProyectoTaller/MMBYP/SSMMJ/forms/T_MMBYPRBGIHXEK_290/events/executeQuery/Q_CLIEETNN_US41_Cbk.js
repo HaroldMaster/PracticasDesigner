84ac189d5b0f8a880898753e67bca158be1256d8
Harold Flores <bhflores1994@gmail.com>
2021-01-11 08:56:34 -0500

    Final

---
+++ ProyectoTaller/MMBYP/SSMMJ/forms/T_MMBYPRBGIHXEK_290/events/executeQuery/Q_CLIEETNN_US41_Cbk.js
@@ -0,0 +1,5 @@
+
+//Start signature to Callback event to Q_CLIEETNN_US41
+task.executeQueryCallback.Q_CLIEETNN_US41 = function(entities, executeQueryCallbackEventArgs) {
+     //here your code
+};
---
+++ ProyectoTaller/MMBYP/SSMMJ/forms/T_MMBYPRBGIHXEK_290/events/executeQuery/Q_PERSONOA_JM48_Cbk.js
@@ -0,0 +1,5 @@
+
+//Start signature to Callback event to Q_PERSONOA_JM48
+task.executeQueryCallback.Q_PERSONOA_JM48 = function(entities, executeQueryCallbackEventArgs) {
+     //here your code
+};
